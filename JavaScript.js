57fdc3d53c909ca61377e5fa8be2a300c775e0aa
manhidnie <manuhidalgonieto@gmail.com>
2020-03-10 14:40:21 +0100

    Entrega de funciones

--- JavaScript.js
+++ JavaScript.js
@@ -1,1 +1,19 @@
-// alert("Gracias por visitar mi página web de cómics")   
+function email() {
+    var email = document.getElementById("email").value;
+    if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/.test(email))) {
+        alert("El correo no sigue la estructura");
+        return false;
+    }
+}
+function telefono() {
+var telefono = document.getElementById("telefono").value;
+    if (isNaN(telefono)){
+    alert("Solo se puede poner números");
+    return false;}
+}
+
+var contraseña = document.getElementById("contraseña").value;
+if (contraseña === null || contraseña === "") {
+    alert("La contraseña está vacía, rellenela");
+    return false;
+}
